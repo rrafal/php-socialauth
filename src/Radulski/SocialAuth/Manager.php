97a15e837f2051645899b95bc40e483b6dca425b
Rafal Radulski <rrafal@gmail.com>
2013-03-04 11:09:45 -0600

    Changed Manager to include class only when loading the provider.

--- src/Radulski/SocialAuth/Manager.php
+++ src/Radulski/SocialAuth/Manager.php
@@ -3,11 +3,6 @@
 namespace Radulski\SocialAuth;
 
 require_once __DIR__ . '/Provider.php';
-require_once __DIR__ . '/Provider/Facebook.php';
-require_once __DIR__ . '/Provider/Google.php';
-require_once __DIR__ . '/Provider/OpenID.php';
-require_once __DIR__ . '/Provider/Twitter.php';
-
 
 class Manager {
 	private $base_url;
@@ -44,22 +39,27 @@ class Manager {
 		$this->provider_map[$name] = array('class'=>$class, 'config'=>$config);
 	}
 	public function configProviders($config){
-		$classes = array(
-			'facebook' => 'Radulski\SocialAuth\Provider\Facebook',
-			'google' => 'Radulski\SocialAuth\Provider\Google',
-			'openid' => 'Radulski\SocialAuth\Provider\OpenID',
-			'twitter' => 'Radulski\SocialAuth\Provider\Twitter',
+		$defaults = array(
+			'facebook' => array('class' => 'Radulski\SocialAuth\Provider\Facebook', 'file' => __DIR__ .'/Provider/Facebook.php'),
+			'google' =>  array('class' => 'Radulski\SocialAuth\Provider\Google', 'file' => __DIR__ .'/Provider/Google.php'),
+			'openid' =>  array('class' => 'Radulski\SocialAuth\Provider\OpenID', 'file' => __DIR__ .'/Provider/OpenID.php'),
+			'twitter' =>  array('class' => 'Radulski\SocialAuth\Provider\Twitter', 'file' => __DIR__ .'/Provider/Twitter.php'),
 		);
 		
 		foreach($config as $name => $provider_config){
+			$file = null;
 			if( isset($provider_config['class']) ){
 				$class = $provider_config['class'];
-			} elseif( isset($classes[ $name ]) ){
-				$class = $classes[$name];
+			} elseif( isset($defaults[ $name ]) ){
+				$class = $defaults[$name]['class'];
+				$file = $defaults[$name]['file'];
 			} else {
 				throw new \Exception("Provider not known: $name");
 			}
 			
+			if($file){
+				include_once $file;
+			}
 			$this->registerProvider($name, $class, $provider_config);
 		}
 	}	
