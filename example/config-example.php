30e153ea88dba18cf6dcc10109b05a02aeba30a0
Rafal Radulski <rrafal@gmail.com>
2013-03-04 18:27:19 -0600

    Updated example config

--- example/config-example.php
+++ example/config-example.php
@@ -3,6 +3,10 @@
 // copy to config.php and complete
 
 $config = array();
+$config['aol'] = array(
+	'storage_type' => 'file', 
+	'storage_path' => '/tmp/social_auth/',
+);
 $config['facebook'] = array(
 	'app_id' => 'ENTER_YOURS', 
 	'secret' => 'ENTER_YOURS',
@@ -19,5 +23,10 @@ $config['twitter'] = array(
 	'consumer_key' => 'ENTER_YOURS', 
 	'consumer_secret' => 'ENTER_YOURS',
 	);
+$config['yahoo'] = array(
+	'storage_type' => 'file', 
+	'storage_path' => '/tmp/social_auth/',
+);
+
 
 
